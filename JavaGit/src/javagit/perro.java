4d6b78ad733f8bc00807493c60a519c27adbbab1
martin diaz <martingonzalodiaz77@gmail.com>
2021-10-15 19:08:56 -0300

    primer comit

---
+++ JavaGit/src/javagit/perro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagit;
+
+/**
+ *
+ * @author martin diaz
+ */
+public class perro {
+    public int edad;
+    public String nombre;
+
+    public perro(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+    
+}
